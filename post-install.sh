7a55ac4a234b526a38da9dfab3121fa9be9e1f21
akira <akira.nakano.cbe@gmail.com>
2021-12-23 21:36:03 +0100

    add zramd

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark"
-git clone https://github.com/akirarice/st.git $HOME/repos/github/test/st
-cd $HOME/repos/github/test/st
+git clone https://github.com/akirarice/st.git $HOME/repos/github/st
+cd $HOME/repos/github/st
 doas make clean install
+git clone https://aur.archlinux.org/zramd $HOME/repos/aur/zramd
+cd $HOME/repos/aur/zramd
+makepkg -si
 cd $HOME
+doas systemctl enable --now zramd
 mkdir dotfiles
 git --git-dir=$HOME/dotfiles/ --work-tree=$HOME clone https://github.com/akirarice/dotfiles.git
