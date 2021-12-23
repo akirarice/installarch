f8e7b951422a5125b9099846fcc903569de1a532
akira <akira.nakano.cbe@gmail.com>
2021-12-23 20:27:36 +0100

    first commit

---
+++ gnome.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1	localhost" >> /etc/hosts
+echo "127.0.1.1	arch.localdomain arch" >> /etc/hosts
+echo "permit nopass akira" >> /etc/doas.conf
+useradd -m akira
+passwd akira
+pacman -S grub grub-btrfs efibootmgr networkmanager os-prober linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack gnome gnome-tweaks materia-gtk-theme rofi ttf-liberation pass pass-otp zsh zsh-syntax-highlighting firefox firefox-ublock-origin openssh rsync neofetch htop opendoas
+
+#pacman -S --noconfirm xf86-video-amdgpu
+
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+systemctl enable gdm
+
+pacman -Rns --noconfirm sudo
