b6a0f7488c66a5eaeb1da03ddfbc6f4054b817ee
akira <akira.nakano.cbe@gmail.com>
2021-12-23 23:32:01 +0100

    use standard zram

--- gnome.sh
+++ gnome.sh
@@ -14,8 +14,9 @@ echo "127.0.1.1	arch.localdomain arch" >> /etc/hosts
 echo "permit nopass akira" >> /etc/doas.conf
 passwd
 useradd -m akira
+usermod -aG wheel akira
 passwd akira
-pacman -S grub grub-btrfs efibootmgr networkmanager os-prober linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack gnome gnome-tweaks materia-gtk-theme rofi ttf-liberation pass pass-otp zsh zsh-syntax-highlighting firefox firefox-ublock-origin openssh rsync neofetch htop opendoas
+pacman -S zram-generator grub grub-btrfs efibootmgr networkmanager os-prober linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack gnome gnome-tweaks materia-gtk-theme rofi ttf-liberation pass pass-otp zsh zsh-syntax-highlighting firefox firefox-ublock-origin openssh rsync neofetch htop opendoas
 
 #pacman -S --noconfirm xf86-video-amdgpu
 
@@ -27,9 +28,7 @@ pacman -S grub grub-btrfs efibootmgr networkmanager os-prober linux-headers pipe
 #grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable gdm
-
-pacman -Rns --noconfirm sudo
-
+EDITOR=nvim visudo
 #set multilib repo & install steam
 #sed -i '/\[multilib\]/s/^#//;/^\[multilib]/{n;s/^#//}' /etc/pacman.conf
 #pacman -Syy
--- post-install.sh
+++ post-install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+#Setup swap
+doas echo [zram0] >> /etc/systemd/zram-generator.conf
+doas systemctl daemon-reload
+doas systemctl start /dev/zram0
+#set theme
 gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark"
+#get simple terminal
 git clone https://github.com/akirarice/st.git $HOME/repos/github/st
 cd $HOME/repos/github/st
 doas make clean install
-git clone https://aur.archlinux.org/zramd $HOME/repos/aur/zramd
-cd $HOME/repos/aur/zramd
+git clone https://aur.archlinux.org/yay.git $HOME/repos/aur/yay
+cd $HOME/repos/aur/yay
 makepkg -si
 cd $HOME
-doas systemctl enable --now zramd
+yay -S libxft-bgra timeshift timeshift-autosnap
 git init --bare $HOME/dotfiles
 alias config='/usr/bin/gt --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 config remote add origin https://github.com/akirarice/dotfiles.git
