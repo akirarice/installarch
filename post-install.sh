8f4de47f0ed01533535531d9aa3c3fe536dff39a
akira <akira.nakano.cbe@gmail.com>
2021-12-23 21:25:26 +0100

    update

--- post-install.sh
+++ post-install.sh
@@ -4,3 +4,5 @@ git clone https://github.com/akirarice/st.git $HOME/repos/github/test/st
 cd $HOME/repos/github/test/st
 doas make clean install
 cd $HOME
+mkdir dotfiles
+git --git-dir=$HOME/dotfiles/ --work-tree=$HOME clone https://github.com/akirarice/dotfiles.git
